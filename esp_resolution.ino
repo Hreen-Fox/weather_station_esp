69670b96a13b15885147ed5bc77afce61057391b
Niki <hreenfox03@gmail.com>
2026-02-01 16:13:01 +0300

    Common commit

---
+++ esp_resolution.ino
@@ -0,0 +1,35 @@
+#include "config_manager.h"
+#include "setup_web.h"
+// web_ui.h подключается автоматически через setup_web.h
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  
+  Serial.println("\n=== ESP8266 Setup Mode ===");
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  
+  loadConfig();
+  
+  if (isValidConfig()) {
+    Serial.println("✅ Найдена валидная конфигурация");
+    Serial.print("SSID: ");
+    Serial.println(config.wifiSSID);
+  }
+  
+  initSetupWebServer();
+}
+
+void loop() {
+  handleSetupRequests();
+  
+  static unsigned long lastBlink = 0;
+  static bool ledState = false;
+  if (millis() - lastBlink > 500) {
+    lastBlink = millis();
+    ledState = !ledState;
+    digitalWrite(LED_BUILTIN, ledState ? LOW : HIGH);
+  }
+}
---
+++ setup_web.h
@@ -0,0 +1,11 @@
+#ifndef SETUP_WEB_H
+#define SETUP_WEB_H
+
+#include <ESP8266WebServer.h>
+
+extern ESP8266WebServer server;
+
+void initSetupWebServer();
+void handleSetupRequests();
+
+#endif
